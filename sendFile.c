c159d70ee7c0494cc0bdfba2a6ba3999404e2e08
Felix Limanta <13515065@std.stei.itb.ac.id>
2017-10-16 20:48:25 +0700

    Send file completed
    Not yet tested, receive must be fixed first

--- sendFile.c
+++ sendFile.c
@@ -5,37 +5,73 @@
 #include <stdlib.h>
 #include <sys/socket.h>
 #include <sys/stat.h>
+#include <unistd.h>
 #include "frame.h"
 
 #define PORT 8000
+#define TIMEOUT 10
 
-void handleError(char* msg);
+//////////////////////////////////
+// Function Forward Declaration //
+//////////////////////////////////
+
+/**
+ * Displays error message to stderr, then exits program with exitcode 1
+ *
+ * @param msg Error message
+ */
+void handleError(const char* msg);
+
+/**
+ * Calculates size of file with path fileName
+ *
+ * @param fileName Path to file
+ */
 off_t fsize(const char* fileName);
+
+/**
+ * Sends file to receiver using the Sliding Window protocol
+ */
 void* sendFile();
+
+/**
+ * Accepts acknowledgement from receiver, then advances LAR accordingly
+ */
 void* receiveAck();
+
+/**
+ * Resets status of frames sent but not yet acknowledged
+ *
+ * Note: statusTable can have three values: -1, 0, 1
+ * -1: frame loaded to buffer, not yet sent or timed out
+ * 0: frame sent
+ * 1: frame acknowledged
+ */
 void* timeout();
 
-// Global variables
+//////////////////////////////////
+// Global Variables Declaration //
+//////////////////////////////////
+
 int my_port, my_ipadr, my_sock;
-int dest_port, dest_ipadr, dest_sock;
-int recv_len;
+int slen;
 int windowSize, bufferSize;
 off_t fileSize;
 struct sockaddr_in socket_sender, socket_destination;
-char msg[6];
 FILE* fptr;
-char* fileName;
 char* senderBuffer;
-Segment* sentSegment;
+char* statusTable;
 Segment** windowBuffer;
-ACK* ack;
 
 // Runtime variables
-int lfs, lar, windowBufferPtr;
+int windowBufferPtr;
+uint32_t lfs, lar;
 uint32_t seqnum;
-char finish = 0;
+
 
 int main (int argc, char* argv[]) {
+	int dest_port, dest_ipadr, dest_sock;
+	char* fileName;
 	pthread_t tidSend, tidReceiveAck, tidTimeout;
 
 	if (argc != 6) {
@@ -63,9 +99,10 @@ int main (int argc, char* argv[]) {
 		handleError("Error: Unable to open file\n");
 	}	
 
-	// Set buffer	
+	// Set buffer
 	senderBuffer = (char*) malloc(bufferSize * sizeof(char));
 	windowBuffer = (Segment**) malloc(windowSize * sizeof(Segment*));
+	statusTable = (char*) malloc(windowSize * sizeof(char));
 
 	// Init runtime vars
 	lfs = -1;
@@ -101,6 +138,7 @@ int main (int argc, char* argv[]) {
 	socket_destination.sin_family = AF_INET;
 	socket_destination.sin_port = htons(dest_port);
 	socket_destination.sin_addr.s_addr = dest_ipadr;
+	slen = sizeof(socket_destination);
 
 	printf("Sending to %s\n	", inet_ntoa(socket_destination.sin_addr));	
 	
@@ -111,17 +149,17 @@ int main (int argc, char* argv[]) {
 
 	pthread_join(tidSend, NULL);
 	pthread_join(tidReceiveAck, NULL);
+	pthread_join(tidTimeout, NULL);
 
 	fclose(fptr);
 	return 0;
 }
 
+///////////////////////////////
+//  Function Implementation  //
+///////////////////////////////
 
-
-
-
-
-void handleError(char* msg) {
+void handleError(const char* msg) {
 	fprintf(stderr, "%s", msg);
 	exit(1);
 }
@@ -137,29 +175,59 @@ off_t fsize(const char* fileName) {
 }
 
 void* sendFile() {
+	int sent_len;
+	Segment* sentSegment;
+	char finish = 0;
+
 	while (!feof(fptr)) {
 		// Read file
 		fread(senderBuffer, bufferSize, 1, fptr);
 
-		windowBufferPtr = 0;
-		for (int i = 0; i < bufferSize && seqnum < fileSize; ++i) {
-			// Convert data to segment
-			sentSegment = (Segment*) malloc(sizeof(Segment));
-			initSegment(sentSegment, seqnum, senderBuffer[i]);
-			seqnum++;
+		// Initialize status table
+		for (int i = 0; i < windowSize; ++i) {
+			statusTable[i] == 1;
+		}
 
-			// Fill sender buffer
-			windowBuffer[windowBufferPtr] = sentSegment;
-			windowBufferPtr = (windowBufferPtr + 1) % windowSize;
+		windowBufferPtr = 0;
+		int i = 0;
+		while (!finish) {
+			// Convert data to segment
+			if (statusTable[windowBufferPtr] == 1 && i < bufferSize && seqnum < fileSize) {
+				sentSegment = (Segment*) malloc(sizeof(Segment));
+				initSegment(sentSegment, seqnum, senderBuffer[i]);
+				seqnum++;
+				lfs++;
+				i++;
+
+				// Fill sender buffer
+				windowBuffer[windowBufferPtr] = sentSegment;
+				statusTable[windowBufferPtr] = -1;
+			}
 
 			// Send data
-			if (sendto(my_sock, sentSegment, sizeof(sentSegment), 0, (struct sockaddr*) &socket_destination,sizeof(socket_destination)) == -1){
-				handleError("Error: Failed to send data\n");
+			for (int j = 0; j < windowSize; ++j) {
+				if (statusTable[j] == -1) {
+					sentSegment = windowBuffer[j];
+					sent_len = sendto(my_sock, sentSegment, sizeof(sentSegment), 0, (struct sockaddr*) &socket_destination,sizeof(socket_destination));
+					if (sent_len == -1) {
+						handleError("Error: Failed to send data\n");
+					}
+					printf("Data %d sent", seqnum - 1);
+					statusTable[j] = 0;
+				}
 			}
-			lfs++;
 
-			// Wait for ACK
-			while (!(lfs - lar <= windowSize));
+			// Advance if smaller than window size and there's still data to send
+			if (lfs - lar <= windowSize && i < bufferSize && seqnum < fileSize) {
+				windowBufferPtr = (windowBufferPtr + 1) % windowSize;
+			}
+
+			// Finish condition: all data sent successfully
+			finish = 1;
+			for (int j = 0; j < windowSize; ++j) {
+				finish &= (statusTable[j] == 1);
+			}
+			finish |= (lar == fileSize - 1);
 		}
 	}
 
@@ -167,12 +235,34 @@ void* sendFile() {
 }
 
 void* receiveAck() {
+	int recv_len;
+	ACK* ack;
 
+	while (lar < fileSize - 1) {
+		// Receive ACK
+		recv_len = recvfrom(my_sock, (char*) ack, 7, 0, (struct sockaddr*) &socket_destination, &slen);
+		if (recv_len != -1) {
+			// Check if ACK is in order
+			statusTable[(ack->nextseq - 1) % windowSize] = 1;
+			if (ack->nextseq == lar + 2) {
+				while (statusTable[(lar + 1) % windowSize] == 1)
+					lar++;
+			}
+		}
+	}
 
 	pthread_exit(NULL);
 }
 
 void* timeout() {
+	while (lar < fileSize - 1) {
+		sleep(TIMEOUT);
+		for (int i = 0; i < windowSize; ++i) {
+			if (statusTable[i] == 0)
+				statusTable[i] = -1;
+		}
+	}
+
 	pthread_exit(NULL);
 }
 
